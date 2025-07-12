dfc7b3ef963711befa039cba340d2f70db4cd4b5
Electrizard <rollingskyisawsome@gmail.com>
2025-07-12 15:32:24 -0400

    implement api logic to translate from english to chinese

--- src/LanguageInput.jsx
+++ src/LanguageInput.jsx
@@ -1,17 +1,28 @@
 import "./LanguageInput.css";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 function LanguageInput({language}){
 
     const [text, setText ] = useState("");
     async function getData(){
-        const response = await fetch(`https://api.mymemory.translated.net/get?q=${text}&langpair=en|zh`);
+        const response = await fetch(`https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=en|zh`);
+        const responseJson = await response.json();
+        const translation = responseJson.responseData.translatedText;
+        console.log(translation);
     }
+
+    function handleChange(e){
+        setText(e.target.value);
+    }
+
+    useEffect(() =>{
+        getData();
+    }, [text]);
 
     return(
         <div className="language-div">
             <h1>{language}</h1>
             <hr></hr>
-            <textarea placeholder={`Translate ${language}`}></textarea>
+            <textarea onChange={(e) => handleChange(e)}placeholder={`Translate ${language}`}></textarea>
         </div>
     );
 }
